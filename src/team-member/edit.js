0527c7b59f4fa45eaf05323b95962015f8d01f2f
monirul-fahad <immonirulfahad@gmail.com>
2023-02-28 08:36:01 +0600

    push

---
+++ src/team-member/edit.js
@@ -0,0 +1,42 @@
+import {
+	useBlockProps,
+	RichText,
+	MediaPlaceholder,
+} from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+export default function Edit({ attributes, setAttributes }) {
+	const { name, bio } = attributes;
+	const onChangeName = (newName) => {
+		setAttributes({ name: newName });
+	};
+	const onChangeBio = (newBio) => {
+		setAttributes({ bio: newBio });
+	};
+	return (
+		<div {...useBlockProps()}>
+			<MediaPlaceholder
+				icon="admin-users"
+				// onSelect={(val) => console.log(val)}
+				// onSelectURL={(val) => console.log(val)}
+				// onError={(err) => console.log(err)}
+				accept="image/*"
+				allowedTypes={['image']}
+			/>
+			<RichText
+				placeholder={__('Member Name', 'team-member')}
+				tagName="h4"
+				onChange={onChangeName}
+				value={name}
+				allowedFormats={[]}
+			/>
+			<RichText
+				placeholder={__('Member Bio', 'team-member')}
+				tagName="p"
+				onChange={onChangeBio}
+				value={bio}
+				allowedFormats={[]}
+			/>
+		</div>
+	);
+}
